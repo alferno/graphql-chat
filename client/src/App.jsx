61a7af68b752e8ee8f4276d70fa94e982ed0c618
Krishan K Yadav <aniandp@gmail.com>
2021-06-24 16:06:22 +0530

    webpack

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,5 @@
 import React from 'react'
 
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'shards-ui/dist/css/shards.min.css'
-import './index.css'
-
 import Chat from './Chat'
 
 const App = () => <Chat />
--- client/src/index.js
+++ client/src/index.js
@@ -2,4 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'shards-ui/dist/css/shards.min.css'
+import './index.css'
+
 ReactDOM.render(<App />, document.getElementById('app'))
