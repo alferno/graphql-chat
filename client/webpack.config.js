210de408fc7494e2381311f7d41430038589f0c4
Krishan K Yadav <aniandp@gmail.com>
2021-06-24 15:30:34 +0530

    webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   entry: './src/index.js',
 
   output: {
-    path: __dirname + '/dist',
+    path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
     filename: 'bundle.js',
   },
