4f4758730c9258820df0365cc3424dd1291d276c
Krishan K Yadav <aniandp@gmail.com>
2021-06-24 16:03:33 +0530

    webpack

--- client/webpack.common.js
+++ client/webpack.common.js
@@ -6,6 +6,28 @@ module.exports = {
   entry: {
     app: './src/index.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.m?js/,
+        type: 'javascript/auto',
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
   plugins: [
     new ModuleFederationPlugin({
       name: 'chat',
